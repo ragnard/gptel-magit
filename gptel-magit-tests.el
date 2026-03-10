19eb629da1133e39dbc64a04bcb6dc6dc216f423
Enze Chi <Enze.Chi@gmail.com>
2026-03-10 12:31:49 +1100

    test(tests): Initialize test file gptel-magit-tests.el

---
+++ gptel-magit-tests.el
@@ -0,0 +1,13 @@
+;;; gptel-magit-tests.el --- Tests for gptel-magit -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2026 Authors
+;; SPDX-License-Identifier: Apache-2.0
+
+;;; Code:
+
+(require 'ert)
+(require 'git-commit)
+(require 'gptel-magit)
+
+(provide 'gptel-magit-tests)
+;;; gptel-magit-tests.el ends here
